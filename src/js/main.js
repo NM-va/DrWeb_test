4231e83a4101001c621e4f133075be5c70b97b55
NM-va <nad.n.mwa@gmail.com>
2026-04-16 12:57:55 +0300

    added icons

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
+import "virtual:svg-icons-register";
 import "../scss/style.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
-  
+
 });
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+import path from "node:path";
 import { defineConfig } from "vite";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 export default defineConfig({
   root: "src",
   publicDir: "../public",
+  plugins: [
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), "src/assets/img/icons")],
+      symbolId: "icon-[dir]-[name]",
+    }),
+  ],
   server: {
     host: true,
     port: 5173,
